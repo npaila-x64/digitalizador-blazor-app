2d0229fda12f6916876b7c53a7470db029eb8104
Nicolás Paila <n.paila01@ufromail.cl>
2023-12-03 10:29:16 -0300

    feat: add image transcription processing

--- DigitalizadorFormularioServer/Services/ImageService.cs
+++ DigitalizadorFormularioServer/Services/ImageService.cs
@@ -1,19 +1,22 @@
 using Microsoft.AspNetCore.Components.Forms;
 using System.Text.Json;
 using System.Net.Http.Json;
+using Newtonsoft.Json;
 
 public class ImageService
 {
     private readonly FormularioService _formularioService;
+    private readonly OpenAIService _openAIService;
     private MemoryStream _fileStream;
     private FormularioTranscription formulario;
     public string ImageBase64 { get; private set; }
     public event Action OnImageProcessed;
 
 
-    public ImageService(FormularioService formularioService)
+    public ImageService(FormularioService formularioService, OpenAIService openAIService)
     {
         _formularioService = formularioService;
+        _openAIService = openAIService;
     }
 
     public async Task StoreFileAsync(IBrowserFile file)
@@ -48,23 +51,18 @@ public class ImageService
 
     public async Task TranscribeImage()
     {
-        var obj = new
-        {
-            content = ImageBase64,
-        };
+        var response = await _openAIService.GetResponseString(ImageBase64);
+        Console.WriteLine(response);
+        Console.WriteLine(ProcessJson(response));
+        _formularioService.FormularioData = ProcessJson(response);
+    }
 
-        JsonContent content = JsonContent.Create(obj);
+    public FormularioTranscription ProcessJson(string jsonEnclosedByThreeBackticks)
+    {
+        string json = jsonEnclosedByThreeBackticks.Replace("```", "").Replace("json", "").Trim();
+        var formulario = JsonConvert.DeserializeObject<FormularioTranscription>(json);
 
-        var httpClient = new HttpClient();
-        var response = await httpClient.PostAsync("http://localhost:30301/image", content);
-        if (response.IsSuccessStatusCode)
-        {
-            formulario = await JsonSerializer.DeserializeAsync<FormularioTranscription>(await response.Content.ReadAsStreamAsync());
-            _formularioService.FormularioData = formulario;
-        }
-        else
-        {
-
-        }
+        // Return a new instance if deserialization returns null
+        return formulario ?? new FormularioTranscription(); 
     }
 }
